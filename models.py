b126c2f29b713e3fddeb0b8dbf78b4c33654b18a
Keane O'Kelley <keane.m.okelley@gmail.com>
2016-10-14 01:23:40 -0500

    Initial Commit

---
+++ models.py
@@ -0,0 +1,41 @@
+import requests
+from peewee import *
+
+from utils import IMDB_API
+
+DEBUG = True
+
+if DEBUG:
+    db = SqliteDatabase('app.db')
+else:
+    db = MySQLDatabase(host="localhost", database="movies", user="movies", passwd="changeme")
+
+
+def db_init():
+    db.connect()
+    try:
+        db.create_tables([Movie])
+        print('Creating tables...')
+    except OperationalError:
+        pass
+    db.close()
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Movie(BaseModel):
+    name = CharField(unique=True)
+    genre = CharField()
+    imdb_id = CharField(max_length=16)
+    watched = BooleanField(default=False)
+
+    def get_details(self):
+        url = IMDB_API + '&i={}'.format(self.imdb_id)
+        r = requests.get(url).json()
+        return """
+        *{}* - _{}_
+        _{}_
+        {}""".format(r['Title'], r['Year'], r['Plot'], r.get('Poster', ''))
---
+++ utils.py
@@ -0,0 +1,38 @@
+import requests
+
+from models import Movie
+
+GOOGLE_API_KEY = 'AIzaSyCa349yW3-oWMbYRHl21V1IgGRyM6O7PW4'
+GOOGLE_CX = '013724138961804180930:qcvlhom_j4e'
+IMDB_API = 'http://www.omdbapi.com/?type=movie'
+
+
+def help_text():
+    return """
+    Invalid command. The following commands are recognized:
+    _choose [category]_ - Select a random movie in the given category. To pick from all categories, no extra argument is necessary.
+    _watched [movie]_ - Mark a movie as watched. This will prevent it from being selected again.
+    _add [movie]_ [-y year] - Add a movie to the list that you'd like to watch in the future.
+    """
+
+
+def add_movie(movie_title, year=""):
+    url = IMDB_API + '&t={}&y='.format(movie_title, year)
+    r = requests.get(url).json()
+    if r.get('Response') == 'False':
+        print("ombdapi failed, falling back to Google")
+        return """
+        Oops, couldn't get an exact hit. Check your spelling and try again. Does this help?
+        {}""".format(custom_google_search(movie_title + " " + year))
+    return Movie.create(name=r['Title'], genre=r['Genre'], imdb_id=r['imdbID'])
+
+
+def custom_google_search(query):
+    url = 'https://www.googleapis.com/customsearch/v1?key={}&cx={}&q={}'.format(
+        GOOGLE_API_KEY, GOOGLE_CX, query
+    )
+    r = requests.get(url).json()
+    response = ""
+    for item in r['items'][:3]:
+        response += "{} -- {}".format(item.get('title'), item.get('snippet'))
+        print(item.get('htmlTitle'), item.get('snippet'))
